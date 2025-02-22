98b2bfd3029cac2f8ed432dd55ae442a9ebc53ed
Sahus Nulu <53015701+Insainian@users.noreply.github.com>
2025-02-21 17:51:24 -0800

    Implement Docker PostgreSQL and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        const ormConfig = configService.get('typeorm');
+        if (!ormConfig) {
+          throw new Error('TypeORM configuration not found');
+        }
+        return {
+          ...ormConfig,
+          // double check port is a number
+          port: Number(ormConfig.port),
+        } as TypeOrmModuleOptions;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
